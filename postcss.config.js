0f82a169fffbfd136bf9fd9957a54d96fa3f8cba
TheBoringDude <iamcoderx@gmail.com>
2020-08-25 20:20:21 +0800

    Configure TailwindCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/*.js", "./pages/*.js"],
-    whitelist: ['html', 'body'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  }
-];
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./pages/**/*.js', './components/**/*.js'],
+
+  // make sure css reset isnt removed on html and body
+  whitelist: ['html', 'body'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
     "postcss-import",
